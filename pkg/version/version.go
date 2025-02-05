d75c2b2021d748c9214d18ba18e5c74b3df2605c
Angus Dippenaar <angusdippenaar@gmail.com>
2025-02-05 21:10:58 +0100

    add version info to nix build

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -20,61 +20,31 @@ package version
 import (
 	"fmt"
 	"runtime"
-	"runtime/debug"
 
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
 
-const ourPath = "github.com/ethereum/go-ethereum" // Path to our module
-
 // These variables are set at build-time by the linker when the build is
 // done by build/ci.go.
-var gitCommit, gitDate string
-var versionMajor, versionMinor, versionPatch string
+var gitCommit, version string
 
 var title = cases.Title(language.English)
 
-// VCSInfo represents the git repository state.
-type VCSInfo struct {
-	Commit string // head commit hash
-	Date   string // commit time in YYYYMMDD format
-	Dirty  bool
-}
-
-// VCS returns version control information of the current executable.
-func VCS() (VCSInfo, bool) {
-	if gitCommit != "" {
-		// Use information set by the build script if present.
-		return VCSInfo{Commit: gitCommit, Date: gitDate, Dirty: false}, true
-	}
-	if buildInfo, ok := debug.ReadBuildInfo(); ok {
-		if buildInfo.Main.Path == ourPath {
-			return buildInfoVCS(buildInfo)
-		}
-	}
-	//nolint:exhaustruct
-	return VCSInfo{}, false
-}
-
-// Semantic holds the textual version string for major.minor.patch.
-var Semantic = fmt.Sprintf("%s.%s.%s", versionMajor, versionMinor, versionPatch)
-
-func VersionWithCommit(gitCommit, gitDate string) string {
-	vsn := Semantic
+func VersionWithCommit() string {
 	if len(gitCommit) >= 8 {
-		vsn += "-" + gitCommit[:8]
+		return version + "-" + gitCommit[:8]
 	}
-	return vsn
+
+	return version + "-" + "dev"
 }
 
 // ClientName creates a software name/version identifier according to common
 // conventions in the Ethereum p2p network.
 func ClientName(clientIdentifier string) string {
-	git, _ := VCS()
 	return fmt.Sprintf("%s/v%v/%v-%v/%v",
 		title.String(clientIdentifier),
-		VersionWithCommit(git.Commit, git.Date),
+		VersionWithCommit(),
 		runtime.GOOS, runtime.GOARCH,
 		runtime.Version(),
 	)
